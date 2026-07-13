dbcd203d2876c15f4a9dfdef7905097217e6b412
Anthony Liddle <anthonyliddle@gmail.com>
2026-07-12 19:59:14 -0700

    feat(ui): self-hosted baloo 2 and nunito, bundled locally

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+import '@fontsource/baloo-2/500.css'
+import '@fontsource/baloo-2/700.css'
+import '@fontsource/nunito/400.css'
+import '@fontsource/nunito/600.css'
+import '@fontsource/nunito/700.css'
 import { createRoot } from 'react-dom/client'
 import { App } from './ui/App'
 import { browserServices } from './ui/services'
